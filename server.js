2623eda78c0c303465d54a9593af6ef66a112a05
Davin Lin <lindavin@grinnell.edu>
2020-06-16 22:07:46 -0400

    Add static files route.
    To do this, we added used express.static and moved the public folder immediately
    out of the views folder.

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+// server.js
+
+//================= Load Modules =========================
 const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const passport = require("passport");
-const passportLocal = require("passport-local-mongoose");
 const flash = require("connect-flash");
 LocalStrategy = require("passport-local").Strategy;
 const database = require('./app/database');
-
-LocalStrategy = require("passport-local").Strategy;
-
 const app = express();
 
+//================== Set View Engine =======================
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +25,7 @@ app.use(
   })
 );
 
+//=================== Setup Passport =======================
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -32,12 +33,13 @@ app.use(passport.session());
 passport.serializeUser(database.User.serializeUser());
 passport.deserializeUser(database.User.deserializeUser());
 
-// passport/login.js
-
 require('./app/loginStrategy')(passport, database.User);
 
 require('./app/signupStrategy')(passport, database.User);
 
+//=================== Routes ================================
 require('./app/routes')(app, passport, database);
 
+//=================== Serving Static Files ==================
+app.use(express.static("public"));
 
