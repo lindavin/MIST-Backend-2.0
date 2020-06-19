6936322b42aff3ec5fb17fc55a9f625430e9c9cf
Davin Lin <lindavin@grinnell.edu>
2020-06-19 18:47:26 -0400

    Complete schemas.
    We added the rest of the schemas that we would need, along with the respective mogoose models.

--- app/database.js
+++ app/database.js
@@ -9,26 +9,76 @@ mongoose.connect("mongodb://localhost:27017/usersDB", {
 
 // Schemas
 const usersSchema = new mongoose.Schema({
+    //objectId: String,                 //aka user id
+    forename: String,
+    surname: String,
     email: String,
-    fname: String,
-    lname: String,
-    username: String,
-    password: String,
-    dateJoined: String
+    userName: String,
+    password: String,                //encrypted
+    createdAt: Date,
+    updatedAt: Date,
+    verified: Boolean,
+    images: Array,
+    albums: Array,
+    workspaces: Array,
+    flag: Boolean,
+    liked: Array,
+    comments: Array, //(of commentIds)
+});
+
+const imagesSchema = new mongoose.Schema({
+    imagesName: String,
+    userId: Object,
+    code: String,
+    ratings: Number,
+    createdAt: Date,
+    comments: Array, //CommentId
+    flag: Boolean,
+    publicity: Number,
+    caption: String,
+});
+
+const commentsSchema = new mongoose.Schema({
+    userId: Object,
+    commentId: String,
+    body: String,
+    createdAt: Date,
+    flagged: Boolean,
+    imageId: Object,
+});
+
+const albumsSchema = new mongoose.Schema({
+    name: String,
+    userId: Object,
+    // albumId: String, automatically generating
+    publicity: Number,
+    createdAt: Date,
+    updatedAt: Date,
+    images: Array,                      // (of Ids)
+    flag: Boolean,
+    caption: String,
 });
 
 const challengeSchema = new mongoose.Schema({
-    category: String,
-    position: String,
-    createdAt: String,
-    modifiedAt: String,
-    title: String,
-    name: String,
+    userId: Object,   
+    name:String,
+    title:String,
+    code:String,
+    createdAt:String,
+    updatedAt:String,
+    flag:Boolean,
+    category:String, // (Beginning,Intermediate,Advanced)(Greyscale,RGB)(Static,Animated)
+    position:String,
     description: String,
-    code: String,
-    rating: Number,
+
 });
 
+const workspacesSchema = new mongoose.Schema({
+    name: String,
+    data: Object,
+    createdAt: Date,
+    updatedAt: Date,
+});
 
 
 // Configuring Schemas
@@ -36,9 +86,17 @@ usersSchema.plugin(passportLocal);
 
 // Models
 const User = mongoose.model("User", usersSchema);
+const Image = mongoose.model("Image", imagesSchema);
+const Comment = mongoose.model("Comment", commentsSchema);
+const Album = mongoose.model("Album", albumsSchema);
 const Challenge = mongoose.model("Challenge", challengeSchema);
+const Workspace = mongoose.model("Workspace", workspacesSchema);
 
 module.exports.User = User;
+module.exports.Image = Image;
+module.exports.Comment = Comment;
+module.exports.Album = Album;
 module.exports.Challenge = Challenge;
+module.exports.Workspace = Workspace;
 
 module.exports.sanitize = sanitize; //sanitizes string
